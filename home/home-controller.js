bf1c45709b1c8fb305b39829fe1a472aabe0b039
RamonFern <ramonfernandes9@gmail.com>
2021-04-28 16:34:25 -0300

    Aula 07 - Criando uma Service, Método GET e Servidor

--- home/home-controller.js
+++ home/home-controller.js
@@ -1,11 +1,19 @@
 angular.module('app').controller('HomeController', HomeController);
-HomeController.$inject = ['$location'];
+HomeController.$inject = ['$location', 'CursoService'];
 
-    function HomeController($location){
+    function HomeController($location, CursoService){
         vm = this;
         vm.teste = 'Home 2'
+        vm.clientes = ''
 
-        vm.navegar = function(){
-            $location.path('Cadastro')
+        vm.navegar = function(rota){
+            $location.path(rota)
+        }
+        vm.listarClientes = function(){
+            CursoService.exec_GET().then(function(resposta){
+                if(resposta){
+                    console.log(resposta)
+                }
+            })
         }
     }
